61dad8cfdde373b56035d6c396354689bc1dd803
Needlife1 <needlife124@gmail.com>
2023-02-02 10:52:41 +0200

    Lessons

---
+++ test.js
@@ -0,0 +1,5 @@
+const arrayOfCategories = categories.querySelector('item');
+console.log(`Number of categories: ${arrayOfCategories.length}`);
+
+const arrayOfHeader = categories.querySelector('h2');
+console.log(arrayOfCategories);
---
+++ Штучки.js
@@ -0,0 +1,44 @@
+//-------------------------------------------- Функция для удаления элемента  с масива
+
+// removeItem(itemToRemove) {
+//     В 5 строчке находим индекс элемента который мы будем удалять
+//     const i = this.НАЗВАНИЕ - МАСИВА.indexOf(itemToRemove);
+//     7 строчка удаляет элемент с помошью метода splice i откуда начинаем удалять а 1 это сколько от начяла убераем елементов
+//   this.items.splice(i, 1);
+
+//  ТАКЖЕ МОЖЕМ УДАЛИТЬ С ПООШЬЮ filter:
+
+// removeItem(itemToRemove) {
+// this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+
+// ------------------------------Пример с добавлением символов в строчку с разных сторон --------------------------
+
+// class StringBuilder {
+
+//   constructor(value) {
+//     this.value = value;
+//   }
+
+//   getValue() {
+//     return this.value;
+//   }
+//   padEnd(str) {
+//     this.value = this.value + str;
+//   }
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
