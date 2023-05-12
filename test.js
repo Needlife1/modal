d4289adb79391f289ef9c66d0b884e2f1abadc6e
Visotsky Maxim <needlife124@gmail.com>
2023-05-12 10:14:02 +0300

    train

--- test.js
+++
@@ -1,5 +0,0 @@
-const arrayOfCategories = categories.querySelector('item');
-console.log(`Number of categories: ${arrayOfCategories.length}`);
-
-const arrayOfHeader = categories.querySelector('h2');
-console.log(arrayOfCategories);
